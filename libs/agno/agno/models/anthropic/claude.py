e863a91df91872c6723f852e3225c5100e15fdd1
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-14 09:51:35 -0400

    RabeyaMuna__diff__id_192

--- libs/agno/agno/models/anthropic/claude.py
+++ libs/agno/agno/models/anthropic/claude.py
@@ -3,7 +3,6 @@ from collections.abc import AsyncIterator
 from dataclasses import dataclass
 from os import getenv
 from typing import Any, Dict, List, Optional, Type, Union
-from pathlib import Path
 
 from pydantic import BaseModel
 
@@ -13,7 +12,6 @@ from agno.models.message import Citations, DocumentCitation, Message, UrlCitatio
 from agno.models.response import ModelResponse
 from agno.utils.log import log_error, log_warning
 from agno.utils.models.claude import format_messages
-from agno.media import File
 
 try:
     from anthropic import Anthropic as AnthropicClient
@@ -201,7 +199,7 @@ class Claude(Model):
         """
         try:
             chat_messages, system_message = format_messages(messages)
-            print('--> chat_messages', chat_messages)
+            print("--> chat_messages", chat_messages)
             request_kwargs = self._prepare_request_kwargs(system_message, tools)
 
             return self.get_client().messages.create(
