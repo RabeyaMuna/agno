63c07c1d140cf897d465b480700acf0e159d55dd
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-14 09:52:14 -0400

    RabeyaMuna__diff__id_204

--- libs/agno/agno/team/team.py
+++ libs/agno/agno/team/team.py
@@ -1910,7 +1910,8 @@ class Team:
             )
 
     def _initialize_session_state(self, user_id: Optional[str] = None, session_id: Optional[str] = None) -> None:
-        self.session_state = self.session_state or {}
+        self.session_state = cast(Dict[str, Any], self.session_state) or {}
+        self.team_session_state = cast(Dict[str, Any], self.team_session_state) or {}
         if user_id is not None:
             self.session_state["current_user_id"] = user_id
             self.team_session_state["current_user_id"] = user_id
